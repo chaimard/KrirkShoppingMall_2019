64df29be8f69d99ed37182ec10a1fd0e1c005ea9
MasterUNG <phrombutr@gmail.com>
2018-03-06 14:31:33 +0700

    Create Icon Menu on Toolbar finish

--- app/src/main/java/masterung/androidthai/in/th/krirkshoppingmall/fragment/RegisterFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/krirkshoppingmall/fragment/RegisterFragment.java
@@ -3,10 +3,15 @@ package masterung.androidthai.in.th.krirkshoppingmall.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import masterung.androidthai.in.th.krirkshoppingmall.MainActivity;
 import masterung.androidthai.in.th.krirkshoppingmall.R;
 
 /**
@@ -14,6 +19,61 @@ import masterung.androidthai.in.th.krirkshoppingmall.R;
  */
 
 public class RegisterFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create Toolbar
+        createToolbar();
+
+
+    }   // Main Method
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.itemUpload) {
+            uploadToServer();
+            return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void uploadToServer() {
+
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_register, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    private void createToolbar() {
+
+        setHasOptionsMenu(true);
+
+        Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
+        ((MainActivity) getActivity()).setSupportActionBar(toolbar);
+
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle(getString(R.string.register));
+        ((MainActivity) getActivity()).getSupportActionBar().setSubtitle(getString(R.string.message_have_space));
+
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+    }
 
     @Nullable
     @Override
