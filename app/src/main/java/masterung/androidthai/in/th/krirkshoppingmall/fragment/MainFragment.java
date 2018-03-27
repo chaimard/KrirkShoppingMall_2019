c5e7bc0fe8dffd00a7232f2e0d97b68b8016cdf1
MasterUNG <phrombutr@gmail.com>
2018-03-27 11:52:56 +0700

    Check Space Main Fragment Success

--- app/src/main/java/masterung/androidthai/in/th/krirkshoppingmall/fragment/MainFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/krirkshoppingmall/fragment/MainFragment.java
@@ -6,9 +6,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import masterung.androidthai.in.th.krirkshoppingmall.R;
+import masterung.androidthai.in.th.krirkshoppingmall.utility.MyAlert;
 
 /**
  * Created by masterung on 6/3/2018 AD.
@@ -23,8 +26,39 @@ public class MainFragment extends Fragment{
 //        Register Controller
         registerController();
 
+//        Login Controller
+        loginController();
 
     }   // Main Method
+
+    private void loginController() {
+        Button button = getView().findViewById(R.id.btnLogin);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                EditText userEditText = getView().findViewById(R.id.edtUser);
+                EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+
+                String userString = userEditText.getText().toString().trim();
+                String passwordString = passwordEditText.getText().toString().trim();
+
+                if (userString.isEmpty() || passwordString.isEmpty()) {
+//                    Have Space
+                    MyAlert myAlert = new MyAlert(getActivity());
+                    myAlert.myDialog(getString(R.string.title_have_space),
+                            getString(R.string.message_have_space));
+
+                } else {
+//                    No Space
+
+
+                }
+
+
+            }
+        });
+    }
 
     private void registerController() {
         TextView textView = getView().findViewById(R.id.txtNewRegister);
